3950649441aff562ec3cee538cd405ae1b24da28
Antony-Lucas <lawokahama@gmail.com>
2024-12-03 20:27:59 -0300

    add: ui ready to deploy

--- src/components/loginOrRegister/LoginOrRegister.jsx
+++ src/components/loginOrRegister/LoginOrRegister.jsx
@@ -150,7 +150,7 @@ const LoginOrRegister = () => {
               {loading ? (
                 <ReactLoading
                   type="spin"
-                  color="#157bff"
+                  color="#ffffff"
                   height={20}
                   width={20}
                 />
